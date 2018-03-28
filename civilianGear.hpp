c0dd8d857d46b821fcdaa29cda897a4c51631dc7
JoramD <j.davids@hotmail.nl>
2018-03-28 22:33:35 +0200

    Re-did all arrays (except vehicles)

--- civilianGear.hpp
+++ civilianGear.hpp
@@ -29,47 +29,95 @@
 "CUP_B_T810_Unarmed_CZ_WDL", "CUP_B_T810_Unarmed_CZ_DES", "CUP_B_T810_Refuel_CZ_WDL", "CUP_B_T810_Refuel_CZ_DES", \
 "CUP_B_LR_Transport_GB_D", "CUP_B_LR_Transport_CZ_W", "tacs_Offroad_B_Black"]
 
-#define TAC_CIVILIAN_UNIFORMS ["U_Afghan01NH", "U_Afghan02NH", "U_Afghan03NH", "U_Afghan06NH", "U_I_C_Soldier_Bandit_4_F", \
-"U_I_C_Soldier_Bandit_1_F", "U_I_C_Soldier_Bandit_2_F", "U_I_C_Soldier_Bandit_3_F", "U_I_C_Soldier_Bandit_5_F", "U_C_Man_casual_2_F", \
-"U_C_Man_casual_1_F", "U_C_Man_casual_3_F", "U_MU_PoloshirtPants2", "U_MU_PoloshirtPants1", "tacs_Uniform_Combat_LS_CDBS_GP_TB", \
-"tacs_Uniform_Combat_LS_CLBS_GP_BB", "tacs_Uniform_Combat_LS_CLRS_TP_BB", "tacs_Uniform_Combat_LS_CPS_BP_BB", \
-"tacs_Uniform_Combat_RS_CDBS_GP_TB", "tacs_Uniform_Combat_RS_CLBS_GP_BB", "tacs_Uniform_Combat_RS_CLRS_TP_BB", \
-"tacs_Uniform_Combat_RS_CPS_BP_BB", "U_C_Poloshirt_blue", "U_C_Poloshirt_burgundy", "U_C_Poloshirt_redwhite", \
-"U_C_Poloshirt_salmon", "U_C_Poloshirt_stripped", "U_C_Poloshirt_tricolour", "U_BG_Guerilla2_2", "U_BG_Guerilla2_1", \
-"U_BG_Guerilla2_3", "U_BG_Guerilla3_1", "U_OrestesBody", "U_Marshal", "U_C_man_sport_1_F", "U_C_man_sport_3_F", "U_C_man_sport_2_F", \
-"U_C_Man_casual_6_F", "U_C_Man_casual_4_F", "U_C_Man_casual_5_F", "tacs_Uniform_Polo_CP_LS_TP_OB", "tacs_Uniform_Polo_CP_RS_LP_BB", \
-"tacs_Suit_VIP", "U_C_Journalist", "U_B_Wetsuit", "U_MU_B_GuerillaGarment2", "U_MU_B_GuerillaGarment2_blue", \
-"U_MU_B_GuerillaGarment2_brown", "U_MU_B_GuerillaGarment2_grey", "U_MU_B_GuerillaGarment2_redcamo", "U_MU_B_GuerillaGarment2_orange", \
-"U_MU_B_GuerillaGarment2_skull", "U_MU_B_GuerillaGarment2_yellow", "U_C_ConstructionCoverall_Black_F", "U_C_ConstructionCoverall_Blue_F", \
-"U_C_ConstructionCoverall_Red_F", "U_C_ConstructionCoverall_Vrana_F", "U_C_Mechanic_01_F", "CUP_U_C_Rescuer_01", "U_C_Paramedic_01_F", \
-"CUP_U_C_Pilot_01"]
+#define TAC_CIVILIAN_UNIFORMS ["U_C_Poor_1", "U_C_WorkerCoveralls", "CUP_U_C_Worker_01", "CUP_U_C_Worker_02", \
+"CUP_U_C_Worker_04", "CUP_U_C_Worker_03", "CUP_U_C_Woodlander_04", "CUP_U_C_Woodlander_02", "CUP_U_C_Woodlander_01", \
+"CUP_U_C_Villager_03", "CUP_U_C_Villager_02", "CUP_U_C_Villager_04", "CUP_U_C_Villager_01", \
+"tacs_Uniform_Polo_TP_WS_TP_TB_NoLogo", "tacs_Uniform_Polo_TP_WS_GP_BB_NoLogo", "tacs_Uniform_Polo_TP_WS_LP_BB_NoLogo", \
+"tacs_Uniform_Polo_TP_TS_GP_BB_NoLogo", "tacs_Uniform_Polo_CP_RS_LP_BB", "tacs_Uniform_Polo_CP_LS_TP_OB", \
+"tacs_Uniform_Polo_TP_GS_TP_TB_NoLogo", "tacs_Uniform_Polo_TP_OS_TP_BB_NoLogo", "tacs_Uniform_Polo_TP_OS_LP_BB_NoLogo", \
+"tacs_Uniform_Polo_TP_LS_TP_TB_NoLogo", "tacs_Uniform_Polo_TP_LS_GP_BB_NoLogo", "tacs_Uniform_Polo_TP_BS_TP_TB_NoLogo", \
+"tacs_Uniform_Polo_CP_BS_TP_BB_NoLogo", "tacs_Uniform_Polo_TP_BS_LP_BB_NoLogo", "U_C_Man_casual_5_F", "U_C_Man_casual_4_F", \
+"U_C_Man_casual_6_F", "CUP_U_C_Suit_02", "CUP_U_C_Suit_01", "U_C_man_sport_2_F", "U_C_man_sport_3_F", "U_C_man_sport_1_F", \
+"U_C_Scientist", "CUP_U_O_RUS_Gorka_Green", "CUP_U_C_Rocker_04", "CUP_U_C_Rocker_02", "CUP_U_C_Rocker_03", \
+"CUP_U_C_Rocker_01", "CUP_U_C_Profiteer_04", "CUP_U_C_Profiteer_01", "CUP_U_C_Profiteer_03", "CUP_U_C_Profiteer_02", \
+"CUP_U_C_Priest_01", "CUP_U_C_Policeman_01", "CUP_I_B_PMC_Unit_4", "CUP_I_B_PMC_Unit_1", "CUP_I_B_PMC_Unit_2", \
+"CUP_I_B_PMC_Unit_3", "CUP_U_C_Pilot_01", "U_I_C_Soldier_Para_5_F", "U_C_Paramedic_01_F", "CUP_U_C_Rescuer_01", \
+"CUP_U_C_Mechanic_01", "CUP_U_C_Mechanic_03", "CUP_U_C_Mechanic_02", "U_C_Mechanic_01_F", "U_Marshal", \
+"CUP_U_C_Labcoat_01", "CUP_U_C_Labcoat_03", "CUP_U_C_Labcoat_02", "CUP_O_TKI_Khet_Partug_03", "CUP_O_TKI_Khet_Partug_06", \
+"CUP_O_TKI_Khet_Partug_05", "CUP_O_TKI_Khet_Partug_08", "CUP_O_TKI_Khet_Partug_07", "CUP_O_TKI_Khet_Partug_01", \
+"CUP_O_TKI_Khet_Partug_02", "CUP_O_TKI_Khet_Partug_04", "CUP_O_TKI_Khet_Jeans_03", "CUP_O_TKI_Khet_Jeans_01", \
+"CUP_O_TKI_Khet_Jeans_02", "CUP_O_TKI_Khet_Jeans_04", "U_C_Journalist", "U_OrestesBody", "U_BG_Guerilla3_1", \
+"U_BG_Guerilla2_3", "U_BG_Guerilla2_1", "U_BG_Guerilla2_2", "U_MU_B_GuerillaGarment2_yellow", \
+"U_MU_B_GuerillaGarment2_skull", "U_MU_B_GuerillaGarment2_orange", "U_MU_B_GuerillaGarment2_brown", \
+"U_MU_B_GuerillaGarment2_blue", "U_MU_B_GuerillaGarment2", "U_B_GEN_Soldier_F", "U_B_GEN_Commander_F", \
+"CUP_U_C_Fireman_01", "U_C_Driver_1_yellow", "U_C_Driver_1_white", "U_C_Driver_4", "U_C_Driver_3", "U_C_Driver_1_red", \
+"U_C_Driver_1_orange", "U_C_Driver_1_green", "U_C_Driver_1", "U_C_Driver_2", "U_C_Driver_1_blue", "U_C_Driver_1_black", \
+"U_MU_B_coverall_grey", "U_MU_B_coverall_dirty2", "U_MU_B_coverall_black", "U_C_ConstructionCoverall_Vrana_F", \
+"U_C_ConstructionCoverall_Red_F", "U_C_ConstructionCoverall_Blue_F", "U_C_ConstructionCoverall_Black_F", \
+"U_C_Poloshirt_tricolour", "U_C_Poloshirt_stripped", "U_C_Poloshirt_salmon", "U_C_Poloshirt_redwhite", \
+"U_C_Poloshirt_burgundy", "U_C_Poloshirt_blue", "tacs_Uniform_Combat_RS_CPS_BP_BB", "tacs_Uniform_Combat_RS_CLRS_TP_BB", \
+"tacs_Uniform_Combat_RS_CLBS_GP_BB", "tacs_Uniform_Combat_RS_CDBS_GP_TB", "tacs_Uniform_Combat_LS_CPS_BP_BB", \
+"tacs_Uniform_Combat_LS_CLRS_TP_BB", "tacs_Uniform_Combat_LS_CLBS_GP_BB", "tacs_Uniform_Combat_LS_CDBS_GP_TB", \
+"CUP_U_C_Citizen_03", "CUP_U_C_Citizen_04", "CUP_U_C_Citizen_01", "CUP_U_C_Citizen_02", "U_MU_PoloshirtPants1", \
+"U_MU_PoloshirtPants2", "CUP_U_O_CHDKZ_Lopotev", "U_C_Man_casual_1_F", "U_C_Man_casual_3_F", "U_C_Man_casual_2_F", \
+"U_I_C_Soldier_Bandit_3_F", "U_I_C_Soldier_Bandit_5_F", "U_I_C_Soldier_Bandit_2_F", "U_I_C_Soldier_Bandit_1_F", \
+"U_I_C_Soldier_Bandit_4_F", "U_C_IDAP_Man_TeeShorts_F", "U_C_IDAP_Man_Tee_F", "U_C_IDAP_Man_shorts_F", \
+"U_C_IDAP_Man_casual_F", "U_C_IDAP_Man_Jeans_F", "U_C_IDAP_Man_cargo_F", "CUP_U_I_GUE_Anorak_01", "CUP_U_I_GUE_Anorak_03", \
+"CUP_U_I_GUE_Anorak_02"]
 
-#define TAC_CIVILIAN_HEADGEAR ["", "Afghan_01Hat", "Afghan_02Hat", "Afghan_03Hat", "Afghan_04Hat", "Afghan_05Hat", "Afghan_06Hat", \
-"H_Bandanna_gry", "H_Bandanna_blu", "H_Bandanna_cbr", "H_Bandanna_khk", "H_Bandanna_sgg", "H_Bandanna_sand", "H_Bandanna_surfer", \
-"H_Bandanna_surfer_blk", "H_Bandanna_surfer_grn", "H_Cap_blk", "H_Cap_blu", "H_Cap_grn", "H_Cap_red", "H_Cap_surfer", "H_Hat_brown", \
-"H_Hat_checker", "H_Hat_grey", "H_Hat_tan", "H_Shemag_olive", "H_ShemagOpen_tan", "H_ShemagOpen_khk", "tacs_Hat_Boonie_RangerGreen", \
-"H_Booniehat_khk", "H_Booniehat_oli", "H_Booniehat_tan", "H_Cap_oli", "H_Cap_press", "H_Cap_tan", "H_Cap_khaki_specops_UK", \
-"H_Cap_usblack", "H_Hat_blue", "H_Cap_marshal", "H_Cap_headphones", "H_Helmet_Skate", "H_RacingHelmet_1_black_F", \
-"H_RacingHelmet_1_blue_F", "H_RacingHelmet_2_F", "H_RacingHelmet_1_F", "H_RacingHelmet_1_green_F", "H_RacingHelmet_1_orange_F", \
-"H_RacingHelmet_1_red_F", "H_RacingHelmet_3_F", "H_RacingHelmet_4_F", "H_RacingHelmet_1_white_F", "H_RacingHelmet_1_yellow_F"]
+#define TAC_CIVILIAN_HEADGEAR ["H_Bandanna_gry", "H_Bandanna_blu", "H_Bandanna_cbr", "CUP_H_FR_BandanaGreen", \
+"H_Bandanna_khk", "H_Bandanna_mcamo", "H_Bandanna_sgg", "H_Bandanna_sand", "H_Bandanna_surfer", "H_Bandanna_surfer_blk", \
+"H_Bandanna_surfer_grn", "CUP_H_C_Beanie_01", "CUP_H_C_Beanie_02", "CUP_H_C_Beanie_03", "CUP_H_C_Beanie_04", \
+"CUP_H_SLA_BeanieGreen", "CUP_H_C_Beret_04", "CUP_H_C_Beret_01", "CUP_H_C_Beret_02", "CUP_H_C_Beret_03", "H_Booniehat_khk", \
+"H_Booniehat_oli", "H_Booniehat_tan", "H_Cap_grn_BI", "H_Cap_blk", "H_Cap_Black_IDAP_F", "H_Cap_blu", \
+"CUP_H_PMC_Cap_Burberry", "H_Cap_grn", "CUP_H_PMC_Cap_Grey", "CUP_H_C_MAGA_01", "H_Cap_oli", "H_Cap_Orange_IDAP_F", \
+"H_Cap_press", "H_Cap_police", "H_Cap_red", "H_Cap_surfer", "CUP_H_PMC_Cap_Tan", "H_Cap_tan", "CUP_H_C_TrackIR_01", \
+"H_Cap_White_IDAP_F", "milgp_h_cap_01_CB", "milgp_h_cap_01_khk", "milgp_h_cap_01_RGR", "CUP_H_PMC_Cap_Back_Burberry", \
+"CUP_H_PMC_Cap_Back_Grey", "CUP_H_PMC_Cap_Back_Tan", "milgp_h_cap_backwards_01_CB", "milgp_h_cap_backwards_01_khk", \
+"milgp_h_cap_backwards_01_RGR", "H_EarProtectors_black_F", "H_EarProtectors_orange_F", "H_EarProtectors_red_F", \
+"H_EarProtectors_white_F", "H_EarProtectors_yellow_F", "CUP_H_NAPA_Fedora", "CUP_H_C_Fireman_Helmet_01", \
+"H_Construction_basic_black_F", "H_Construction_earprot_black_F", "H_Construction_headset_black_F", \
+"H_Construction_basic_orange_F", "H_Construction_earprot_orange_F", "H_Construction_headset_orange_F", \
+"H_Construction_basic_red_F", "H_Construction_earprot_red_F", "H_Construction_headset_red_F", \
+"H_Construction_basic_vrana_F", "H_Construction_earprot_vrana_F", "H_Construction_headset_vrana_F", \
+"H_Construction_basic_white_F", "H_Construction_earprot_white_F", "H_Construction_headset_white_F", \
+"H_Construction_basic_yellow_F", "H_Construction_earprot_yellow_F", "H_Construction_headset_yellow_F", "H_Hat_blue", \
+"H_Hat_brown", "H_Hat_checker", "H_Hat_grey", "H_Hat_tan", "H_HeadSet_black_F", "H_HeadSet_orange_F", "H_HeadSet_red_F", \
+"H_HeadSet_white_F", "H_HeadSet_yellow_F", "H_HeadBandage_clean_F", "H_HeadBandage_stained_F", "H_HeadBandage_bloody_F", \
+"H_MilCap_gen_F", "CUP_H_C_Policecap_01", "H_PASGT_basic_blue_press_F", "H_PASGT_neckprot_blue_press_F", \
+"H_RacingHelmet_1_black_F", "H_RacingHelmet_1_blue_F", "H_RacingHelmet_2_F", "H_RacingHelmet_1_F", \
+"H_RacingHelmet_1_green_F", "H_RacingHelmet_1_orange_F", "H_RacingHelmet_1_red_F", "H_RacingHelmet_3_F", \
+"H_RacingHelmet_4_F", "H_RacingHelmet_1_white_F", "H_RacingHelmet_1_yellow_F", "H_Hat_Safari_olive_F"]
 
-#define TAC_CIVILIAN_BACKPACKS ["", "B_AssaultPack_blk", "B_AssaultPack_cbr", "B_AssaultPack_rgr", "B_AssaultPack_khk", \
-"B_AssaultPack_sgg", "B_FieldPack_blk", "B_FieldPack_cbr", "B_FieldPack_khk", "B_FieldPack_oli", "B_MU_Kitbag_blk", "B_Kitbag_cbr", \
-"tacs_Backpack_Kitbag_DarkBlack", "B_Kitbag_rgr", "B_Kitbag_sgg", "ACE_NonSteerableParachute", "B_Parachute", "B_TacticalPack_blk", \
-"B_MU_TacticalPack_cbr", "B_TacticalPack_rgr", "B_TacticalPack_oli", "B_LegStrapBag_olive_F", "B_LegStrapBag_coyote_F", \
-"B_LegStrapBag_black_F", "B_Messenger_Black_F", "B_Messenger_Olive_F", "B_Messenger_Coyote_F", "B_Messenger_Gray_F", \
-"B_Messenger_IDAP_F", "CUP_B_RUS_Backpack"]
+#define TAC_CIVILIAN_BACKPACKS ["CUP_B_AlicePack_Khaki", "CUP_B_AlicePack_Khaki", "B_AssaultPack_blk", "B_AssaultPack_cbr", \
+"B_AssaultPack_rgr", "B_AssaultPack_khk", "B_AssaultPack_sgg", "CUP_B_Bergen_BAF", "B_MU_Carryall_blk", "B_Carryall_cbr", \
+"tacs_Backpack_Carryall_DarkBlack", "B_Carryall_khk", "B_Carryall_oli", "B_FieldPack_blk", "B_FieldPack_cbr", \
+"B_FieldPack_khk", "B_FieldPack_oli", "B_MU_Kitbag_blk", "B_Kitbag_cbr", "tacs_Backpack_Kitbag_DarkBlack", \
+"B_Kitbag_rgr", "B_Kitbag_sgg", "B_LegStrapBag_black_F", "B_LegStrapBag_coyote_F", "B_LegStrapBag_olive_F", \
+"B_Messenger_Black_F", "B_Messenger_Coyote_F", "B_Messenger_Gray_F", "B_Messenger_Olive_F", "B_Messenger_IDAP_F", \
+"CUP_B_RUS_Backpack", "B_TacticalPack_blk", "B_MU_TacticalPack_cbr", "B_TacticalPack_rgr", "B_TacticalPack_oli"]
 
-#define TAC_CIVILIAN_VESTS ["", "V_Press_F", "V_RebreatherB", "lbt_pouchless_od", "lbt_pouchless_coy", "V_DeckCrew_blue_F", \
-"V_DeckCrew_green_F", "V_DeckCrew_red_F", "V_DeckCrew_white_F", "V_DeckCrew_yellow_F", "V_DeckCrew_violet_F", "V_DeckCrew_brown_F", \
-"V_Plain_crystal_F", "V_Plain_medical_F", "V_LegStrapBag_black_F", "V_LegStrapBag_coyote_F", "V_LegStrapBag_olive_F", \
-"V_Safety_blue_F", "V_Safety_orange_F", "V_Safety_yellow_F", "CUP_V_OI_TKI_Jacket1_04", "CUP_V_OI_TKI_Jacket1_05", \
-"CUP_V_OI_TKI_Jacket1_06", "CUP_V_OI_TKI_Jacket5_04", "CUP_V_OI_TKI_Jacket5_05", "CUP_V_OI_TKI_Jacket5_06", \
-"CUP_V_OI_TKI_Jacket6_04", "CUP_V_OI_TKI_Jacket6_05", "CUP_V_OI_TKI_Jacket6_06"]
+#define TAC_CIVILIAN_VESTS ["V_Press_F", "CUP_V_OI_TKI_Jacket6_06", "CUP_V_OI_TKI_Jacket6_05", "CUP_V_OI_TKI_Jacket6_04", \
+"CUP_V_OI_TKI_Jacket5_06", "CUP_V_OI_TKI_Jacket5_02", "CUP_V_OI_TKI_Jacket5_05", "CUP_V_OI_TKI_Jacket5_04", \
+"CUP_V_OI_TKI_Jacket1_05", "CUP_V_OI_TKI_Jacket1_06", "CUP_V_OI_TKI_Jacket1_04", "V_Safety_yellow_F", "V_Safety_orange_F", \
+"V_Safety_blue_F", "V_Pocketed_olive_F", "V_Pocketed_coyote_F", "V_Pocketed_black_F", "V_LegStrapBag_olive_F", \
+"V_LegStrapBag_coyote_F", "V_LegStrapBag_black_F", "V_Plain_medical_F", "V_Plain_crystal_F", "V_TacVest_gen_F", \
+"V_EOD_blue_F", "V_EOD_IDAP_blue_F", "V_EOD_coyote_F", "V_EOD_olive_F"]
 
-#define TAC_CIVILIAN_GLASSES ["", "G_Aviator", "G_Bandanna_aviator", "G_Bandanna_blk", "G_Bandanna_khk", "G_Bandanna_oli", \
-"G_Bandanna_shades", "G_Bandanna_sport", "G_Bandanna_tan", "G_Shades_Black", "G_Shades_Blue", "G_Shades_Green", "G_Shades_Red", \
-"G_Spectacles", "G_Sport_Red", "G_Sport_Blackyellow", "G_Sport_BlackWhite", "G_Sport_Checkered", "G_Sport_BlackredG_Sport_Greenblack", \
-"G_Squares_Tinted", "G_Squares", "G_Spectacles_Tinted", "tacs_goggles_black", "tacs_Goggles_Green", "tacs_Goggles_Tan"]
+#define TAC_CIVILIAN_FACEWEAR ["G_Aviator", "G_Bandanna_aviator", "G_Bandanna_beast", "G_Bandanna_blk", "G_Bandanna_khk", \
+"G_Bandanna_oli", "G_Bandanna_shades", "G_Bandanna_sport", "G_Bandanna_tan", "milgp_f_face_shield_BLK", \
+"milgp_f_face_shield_CB", "milgp_f_face_shield_khk", "milgp_f_face_shield_RGR", "milgp_f_face_shield_shades_BLK", \
+"milgp_f_face_shield_shades_CB", "milgp_f_face_shield_shades_khk", "milgp_f_face_shield_shades_RGR", \
+"milgp_f_face_shield_shades_shemagh_BLK", "milgp_f_face_shield_shades_shemagh_CB", \
+"milgp_f_face_shield_shades_shemagh_khk", "milgp_f_face_shield_shades_shemagh_RGR", "milgp_f_face_shield_shemagh_BLK", \
+"milgp_f_face_shield_shemagh_CB", "milgp_f_face_shield_shemagh_khk", "milgp_f_face_shield_shemagh_RGR", \
+"milgp_f_face_shield_tactical_BLK", "milgp_f_face_shield_tactical_CB", "milgp_f_face_shield_tactical_khk", \
+"milgp_f_face_shield_tactical_RGR", "milgp_f_face_shield_tactical_shemagh_BLK", "milgp_f_face_shield_tactical_shemagh_CB", \
+"milgp_f_face_shield_tactical_shemagh_khk", "milgp_f_face_shield_tactical_shemagh_RGR", "G_Lady_Blue", "CUP_TK_NeckScarf", \
+"G_Respirator_blue_F", "G_Respirator_white_F", "G_Respirator_yellow_F", "G_EyeProtectors_F", "G_EyeProtectors_Earpiece_F", \
+"G_Shades_Black", "G_Shades_Blue", "G_Shades_Green", "G_Shades_Red", "CUP_FR_NeckScarf", "CUP_FR_NeckScarf2", \
+"G_Spectacles", "G_Sport_Red", "G_Sport_Blackyellow", "G_Sport_BlackWhite", "G_Sport_Checkered", "G_Sport_Blackred", \
+"G_Sport_Greenblack", "G_Squares_Tinted", "G_Squares", "milgp_f_tactical_khk", "G_Spectacles_Tinted", \
+"G_WirelessEarpiece_F"]
 
 #define TAC_CIVILIAN_REDZONES ["embassy_marker"]
